f85d5182476f77225eee7d539c10f5090d2773f6
Muhammad Umar Aziz Khan <105535313+MuhammadUmar969@users.noreply.github.com>
2022-12-12 00:16:42 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import TextForm from './components/TextForm'
+
+function App() {
+  return (
+    <>
+   <Navbar title="TextUtils" aboutText="About Us" />
+   <div className="container my-3">
+   <TextForm heading="Enter the text to analyze below"/>
+    </div> 
+   </>
+ 
+  )
+}
+
+export default App
